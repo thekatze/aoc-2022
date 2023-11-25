5598d9bd3b8e7ee23521b2ac7c821def7d747390
Leo Katzengruber <leofel.katz@gmail.com>
2023-11-26 00:27:42 +0100

    day 06 part 1

--- day-06/src/main.rs
+++ day-06/src/main.rs
@@ -1,3 +1,19 @@
+use itertools::Itertools;
+
+const INPUT: &str = include_str!("input.txt");
+
 fn main() {
-    println!("Hello, world!");
+    let index = INPUT
+        .chars()
+        .tuple_windows()
+        .enumerate()
+        .find_map(|(index, (a, b, c, d))| {
+            if a == b || a == c || a == d || b == c || b == d || c == d {
+                None
+            } else {
+                Some(index + 4)
+            }
+        }).expect("string should have 4 consecutive characters that are distinct");
+
+    dbg!(index);
 }
