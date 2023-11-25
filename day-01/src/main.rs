532ae1f3372d75d61470e082678640d48ca40e91
Leo Katzengruber <leofel.katz@gmail.com>
2023-11-25 10:26:12 +0100

    day 01

--- day-01/src/main.rs
+++ day-01/src/main.rs
@@ -1,3 +1,18 @@
+const INPUT: &str = include_str!("input.txt");
+
 fn main() {
-    println!("Hello, world!");
+    let mut sorted = INPUT
+        .split("\n\n")
+        .map(|block| {
+            block
+                .lines()
+                .filter_map(|num| num.parse::<u32>().ok())
+                .sum::<u32>()
+        })
+        .collect::<Vec<u32>>();
+
+    sorted.sort_by(|a, b| b.cmp(a));
+
+    let result: u32 = sorted.iter().take(3).sum();
+    dbg!(result);
 }
